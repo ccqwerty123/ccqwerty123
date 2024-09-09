b9a0c9d4e18ac4c45a323135c49c782f6f130a91
ccqwerty123 <63049747+ccqwerty123@users.noreply.github.com>
2024-09-10 07:55:52 +0800

    Update cheshi.sh

--- cheshi.sh
+++ cheshi.sh
@@ -3,31 +3,21 @@
 # 检查是否为有效的IPv6地址
 is_valid_ipv6() {
     local ip=$1
-    if [[ $ip =~ ^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,7}:$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}$ ]] || 
-       [[ $ip =~ ^[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})$ ]] || 
-       [[ $ip =~ ^:((:[0-9a-fA-F]{1,4}){1,7}|:)$ ]] || 
-       [[ $ip =~ ^fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}$ ]] || 
-       [[ $ip =~ ^::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$ ]] || 
-       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$ ]]; then
-        return 0
-    else
-        return 1
+    if [[ $ip =~ : ]]; then
+        ip -6 addr add $ip/128 dev lo 2>/dev/null
+        if [ $? -eq 0 ]; then
+            ip -6 addr del $ip/128 dev lo
+            return 0
+        fi
     fi
+    return 1
 }
 
 # 获取域名的IPv6地址
 get_ipv6_address() {
     local domain=$1
     local dns_server="2001:4860:4860::6464"  # 默认使用Google的DNS64服务器
-    echo "$dns_server"
-    local result=$(dig AAAA +short @$dns_server $domain)
-    echo "$result"
+    local result=$(dig AAAA +short @$dns_server $domain +timeout=5)
     for ip in $result; do
         if is_valid_ipv6 "$ip"; then
             echo "$ip"
@@ -48,11 +38,10 @@ replace_domain_with_ipv6() {
 }
 
 # 下载文件并计算下载速度
-# 下载文件并计算下载速度
 download_file() {
     local url=$1
     local start=$(date +%s.%N)
-    local size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
+    local size=$(curl -s -L -o /dev/null -w "%{size_download}" "$url")
     local end=$(date +%s.%N)
     local duration=$(echo "$end - $start" | bc)
     if [ -z "$size" ] || [ -z "$duration" ] || [ $(echo "$duration == 0" | bc) -eq 1 ]; then
@@ -63,7 +52,11 @@ download_file() {
 }
 
 main() {
-    read -p "请输入要测试的URL: " url
+    local default_url="https://dldir1.qq.com/qqfile/qq/PCQQ9.7.17/QQ9.7.17.29225.exe"
+    
+    read -p "请输入要测试的URL (按Enter使用默认地址): " url
+    url=${url:-$default_url}
+    
     domain=$(echo $url | sed -e 's,^.*://,,g' | cut -d/ -f1 | cut -d: -f1)
     
     ipv6_address=$(get_ipv6_address $domain)
