0015de34248add7588136b06da192e524c62305c
ccqwerty123 <63049747+ccqwerty123@users.noreply.github.com>
2024-09-10 06:57:10 +0800

    Update cheshi.sh

--- cheshi.sh
+++ cheshi.sh
@@ -43,7 +43,8 @@ query_ipv6() {
         echo "未找到 $domain 的 IPv6 地址"
         return 1
     else
-        echo "$domain 的 IPv6 地址是: $ipv6_address"
+        echo "$domain 的 IPv6 地址是:"
+        echo "$ipv6_address"
         return 0
     fi
 }
@@ -54,14 +55,14 @@ main() {
 
     while true; do
         # 一级菜单：选择查询方式或退出
-        echo "请选择查询方式:"
+        echo "请选择查询方式 (默认是 dig):"
         echo "1) dig"
         echo "2) nslookup"
         echo "3) 退出"
         read -p "请输入选项 (1/2/3): " method_choice
 
         case "$method_choice" in
-            1)
+            1|"")
                 method="dig"
                 ;;
             2)
@@ -78,12 +79,12 @@ main() {
         esac
 
         # 选择查询的域名
-        read -p "请输入要查询的域名 (默认是 sohu.com): " user_domain
-        domain="${user_domain:-sohu.com}"
+        read -p "请输入要查询的域名 (默认是 dldir1.qq.com): " user_domain
+        domain="${user_domain:-dldir1.qq.com}"
 
         # 选择 DNS 服务器
-        read -p "请输入 DNS 服务器 (留空使用默认 DNS): " user_dns
-        dns_server="$user_dns"
+        read -p "请输入 DNS 服务器 (默认是 2001:4860:4860::64): " user_dns
+        dns_server="${user_dns:-2001:4860:4860::64}"
 
         # 查询 IPv6 地址
         query_ipv6 "$domain" "$dns_server" "$method"
