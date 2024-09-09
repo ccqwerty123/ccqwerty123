3610d31532538a045b3af17c4e74103b4cf51817
ccqwerty123 <63049747+ccqwerty123@users.noreply.github.com>
2024-09-10 07:07:26 +0800

    Update cheshi.sh

--- cheshi.sh
+++ cheshi.sh
@@ -13,6 +13,12 @@ install_tools() {
         sudo apt-get update
         sudo apt-get install -y dnsutils
     fi
+
+    if ! command -v curl &> /dev/null; then
+        echo "未找到 curl，正在安装..."
+        sudo apt-get update
+        sudo apt-get install -y curl
+    fi
 }
 
 # 查询 IPv6 地址的函数
@@ -24,15 +30,15 @@ query_ipv6() {
 
     if [ "$method" == "dig" ]; then
         if [ -n "$dns_server" ]; then
-            ipv6_address=$(dig AAAA "$domain" @"$dns_server" +short)
+            ipv6_address=$(dig AAAA "$domain" @"$dns_server" +short | grep -E '^[0-9a-fA-F:]+$')
         else
-            ipv6_address=$(dig AAAA "$domain" +short)
+            ipv6_address=$(dig AAAA "$domain" +short | grep -E '^[0-9a-fA-F:]+$')
         fi
     elif [ "$method" == "nslookup" ]; then
         if [ -n "$dns_server" ]; then
-            ipv6_address=$(nslookup -query=AAAA "$domain" "$dns_server" | grep 'address' | awk '{print $2}')
+            ipv6_address=$(nslookup -query=AAAA "$domain" "$dns_server" | grep 'address' | awk '{print $2}' | grep -E '^[0-9a-fA-F:]+$')
         else
-            ipv6_address=$(nslookup -query=AAAA "$domain" | grep 'address' | awk '{print $2}')
+            ipv6_address=$(nslookup -query=AAAA "$domain" | grep 'address' | awk '{print $2}' | grep -E '^[0-9a-fA-F:]+$')
         fi
     else
         echo "不支持的查询方式: $method"
@@ -43,10 +49,47 @@ query_ipv6() {
         echo "未找到 $domain 的 IPv6 地址"
         return 1
     else
-        echo "$domain 的 IPv6 地址是:"
+        echo "$domain 的 IPv6 地址是: $ipv6_address"
         echo "$ipv6_address"
         return 0
     fi
+}
+
+# 清理指定文件夹中的文件
+clean_directory() {
+    local dir="$1"
+    if [ -d "$dir" ]; then
+        echo "清理文件夹: $dir"
+        rm -rf "$dir"/*
+    else
+        echo "文件夹不存在，正在创建: $dir"
+        mkdir -p "$dir"
+    fi
+}
+
+# 使用 IPv6 地址下载文件并测试速度
+download_with_ipv6() {
+    local ipv6_address="$1"
+    local url="$2"
+    local download_dir="$3"
+
+    # 提取协议和路径
+    local protocol=$(echo "$url" | awk -F'://' '{print $1}')
+    local path=$(echo "$url" | awk -F'://' '{print $2}' | awk -F'/' '{print substr($0, index($0,$2))}')
+
+    # 构造新的URL
+    local new_url="$protocol://[$ipv6_address]/$path"
+
+    # 清理下载目录
+    clean_directory "$download_dir"
+
+    # 下载文件到指定目录
+    local output_file="$download_dir/$(basename "$path")"
+    echo "使用 IPv6 地址下载文件: $new_url"
+    curl -6 -o "$output_file" -w "下载速度: %{speed_download} bytes/sec\n" -L "$new_url"
+
+    # 清理下载目录
+    clean_directory "$download_dir"
 }
 
 # 主程序
@@ -87,7 +130,28 @@ main() {
         dns_server="${user_dns:-2001:4860:4860::64}"
 
         # 查询 IPv6 地址
-        query_ipv6 "$domain" "$dns_server" "$method"
+        if query_ipv6 "$domain" "$dns_server" "$method"; then
+            ipv6_address=$(query_ipv6 "$domain" "$dns_server" "$method")
+
+            # 询问用户是否进行测速
+            echo "是否使用查询到的 IPv6 地址进行测速?"
+            echo "1) 是"
+            echo "2) 否"
+            read -p "请输入选项 (1/2): " speed_test_choice
+
+            if [ "$speed_test_choice" == "1" ]; then
+                # 选择下载目录
+                read -p "请输入下载目录 (默认是 /tmp/download_test): " download_dir
+                download_dir="${download_dir:-/tmp/download_test}"
+
+                # 选择下载的URL
+                read -p "请输入要下载的文件URL (默认是 https://dldir1.qq.com/qqfile/qq/PCQQ9.7.17/QQ9.7.17.29225.exe): " download_url
+                download_url="${download_url:-https://dldir1.qq.com/qqfile/qq/PCQQ9.7.17/QQ9.7.17.29225.exe}"
+
+                # 使用 IPv6 地址下载文件并测试速度
+                download_with_ipv6 "$ipv6_address" "$download_url" "$download_dir"
+            fi
+        fi
 
         # 询问用户是否继续查询
         echo "是否继续查询?"
