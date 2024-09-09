00740cf855a8884cb38b7c191a4076045450215b
ccqwerty123 <63049747+ccqwerty123@users.noreply.github.com>
2024-09-10 07:28:34 +0800

    Update cheshi.sh

--- cheshi.sh
+++ cheshi.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+is_valid_ipv6() {
+    local ip=$1
+    if [[ $ip =~ ^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,7}:$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}$ ]] || 
+       [[ $ip =~ ^[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})$ ]] || 
+       [[ $ip =~ ^:((:[0-9a-fA-F]{1,4}){1,7}|:)$ ]] || 
+       [[ $ip =~ ^fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}$ ]] || 
+       [[ $ip =~ ^::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$ ]] || 
+       [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 get_ipv6_address() {
     local domain=$1
-    dig @2001:4860:4860::6464 AAAA $domain +short
+    local result=$(dig @2001:4860:4860::6464 AAAA $domain +short)
+    for ip in $result; do
+        if is_valid_ipv6 "$ip"; then
+            echo "$ip"
+            return
+        fi
+    done
+    echo ""
 }
 
 replace_domain_with_ipv6() {
@@ -20,8 +47,11 @@ download_file() {
     local size=$(curl -s -o /dev/null -w "%{size_download}" "$url")
     local end=$(date +%s.%N)
     local duration=$(echo "$end - $start" | bc)
-    local speed=$(echo "scale=2; $size / $duration / 1024 / 1024" | bc)
-    echo $speed
+    if [ -z "$size" ] || [ -z "$duration" ] || [ $(echo "$duration == 0" | bc) -eq 1 ]; then
+        echo "error"
+    else
+        echo "scale=2; $size / $duration / 1024 / 1024" | bc
+    fi
 }
 
 main() {
@@ -30,15 +60,21 @@ main() {
     
     ipv6_address=$(get_ipv6_address $domain)
     if [ -z "$ipv6_address" ]; then
-        echo "无法获取 $domain 的IPv6地址"
+        echo "无法获取 $domain 的有效IPv6地址"
         exit 1
     fi
+    
+    echo "$domain 的 IPv6 地址是: $ipv6_address"
     
     ipv6_url=$(replace_domain_with_ipv6 $url $ipv6_address)
     echo "IPv6 URL: $ipv6_url"
     
     speed=$(download_file $ipv6_url)
-    echo "下载速度: ${speed} MB/s"
+    if [ "$speed" = "error" ]; then
+        echo "下载测试失败"
+    else
+        echo "下载速度: ${speed} MB/s"
+    fi
 }
 
 main
